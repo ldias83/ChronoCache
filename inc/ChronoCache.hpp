a0ffd784722f50dde29da61a8b5e53c0481699b6
Lucas <ldias.santos.83@gmail.com>
2025-05-30 18:18:35 +0200

    Make ChronoCache generic - template based

--- inc/ChronoCache.hpp
+++ inc/ChronoCache.hpp
@@ -4,30 +4,32 @@
 
 using Clock = std::chrono::steady_clock;
 
+template <typename K, typename V>
 class ChronoCache {
 public:
 
-    auto put(const std::string& key, const std::string& value, std::chrono::seconds ttl) -> void {
+    auto put(const K& key, const V& value, std::chrono::seconds ttl) -> void {
         auto expiration { Clock::now() + ttl };
         m_cache[key] = { value, expiration };
     }
 
-    auto get(const std::string& key) -> std::optional<std::string> {
-        if (m_cache.find(key) != m_cache.end()) {
-            auto& [value, expiration] = m_cache[key];
+    auto get(const K& key) -> std::optional<V> {
+        auto cacheIt { m_cache.find(key) };
+        if (cacheIt != m_cache.end()) {
+            auto& [value, expiration] = cacheIt->second;
             if (Clock::now() < expiration) {
                 return value;
             }
-            m_cache.erase(key);
+            m_cache.erase(cacheIt);
         }
         return std::nullopt;
     }
 
 private:
     struct Entry {
-        std::string value;
+        V value;
         Clock::time_point expiry;
     };
 
-    std::unordered_map<std::string, Entry> m_cache {};
+    std::unordered_map<K, Entry> m_cache {};
 };
