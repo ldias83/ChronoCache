8aeb4df058b24aeabd96f1e5aff5bfb66a5fa3da
Lucas <ldias.santos.83@gmail.com>
2025-05-30 18:18:35 +0200

    Make ChronoCache generic - template based

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <thread>
 #include "ChronoCache.hpp"
 
-template <typename T>
-void tryGetCache(ChronoCache& cache, const T& key) {
+template <typename K, typename V>
+void tryGetCache(ChronoCache<K, V>& cache, const K& key) {
     if (auto value = cache.get(key)) {
         std::cout << "Found valid token: " << key << ": " << *value << std::endl;
     } else {
@@ -13,12 +13,15 @@ void tryGetCache(ChronoCache& cache, const T& key) {
 
 int main() {
 
-    ChronoCache cache;
-    cache.put("token_001", "123456", std::chrono::seconds(5));
-    tryGetCache(cache, "token_001");
+    std::string token {"token_001"};
+    std::string value {"123456789"};
+    ChronoCache<std::string, std::string> cache;
+
+    cache.put(token, value, std::chrono::seconds(5));
+    tryGetCache(cache, token);
     
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    tryGetCache(cache, "token_001");
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    tryGetCache(cache, token);
 
     return 0;
 }
